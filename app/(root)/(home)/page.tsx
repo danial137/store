f7fa7c3f5806abbd6f844c0ddaf632c26291ad21
danial137 <danial79fakhrabadi@gmail.com>
2025-08-29 16:47:48 +0330

    Add SVG logos for Next.js and Vercel; configure Tailwind CSS and TypeScript settings

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,10 +1,15 @@
+import Home from "@/components/Home/Home";
 import React from "react";
 
 // This Project file only contains the image not shadcn setup and other file setup
 // Watch the setup part of the video to fully understand the setup
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <Home />
+    </div>
+  )
 };
 
 export default HomePage;
