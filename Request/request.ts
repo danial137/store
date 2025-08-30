904383f71ef495d9ed8daf93bfbd43ebaafbb8b1
danial137 <danial79fakhrabadi@gmail.com>
2025-08-30 19:41:42 +0330

    feat: add getProductByCategory function and integrate it into ProductDetails component

--- Request/request.ts
+++ Request/request.ts
@@ -13,3 +13,8 @@ export async function getSingleProduct(id:string ) {
     const singleProductRes = await fetch(`https://fakestoreapi.com/products/${id}`);
     return singleProductRes.json();
 } 
+
+export async function getProductByCategory(category:string) {
+    const productByCategoryRes = await fetch(`https://fakestoreapi.com/products/category/${category}`);
+    return productByCategoryRes  .json();
+}
--- app/(root)/product/product-details/[id]/page.tsx
+++ app/(root)/product/product-details/[id]/page.tsx
@@ -1,19 +1,25 @@
-import { getSingleProduct } from '@/Request/request'
+import { getProductByCategory, getSingleProduct } from '@/Request/request'
+import { Product } from '@/typing'
+import Image from 'next/image'
 import React from 'react'
 
-interface PageProps {
-    params: { id: string }
-}
-
-const ProductDetails = async ({ params }: PageProps) => {
-    const { id } = params
-    const singleProduct = await getSingleProduct(id)
-
+const ProductDetails = async ({ params }: { params: { id: string } }) => {
+    const id = params.id
+    const singleProduct: Product = await getSingleProduct(id)
+    const relatedProduct: Product[] = await getProductByCategory(singleProduct.category);
+    const num = Math.round(singleProduct?.rating?.rate);
+    const starArray = new Array(num).fill(0);
     return (
-        <div>
-            <h1>{singleProduct.name}</h1>
-            <p>{singleProduct.description}</p>
-            <p>Price: ${singleProduct.price}</p>
+        <div className='mt-20'>
+            <div className='w-4/5 mx-auto grid grid-cols-1 lg:grid-cols-7 items-center gap-4 '>
+            
+                <div className='col-span-3 mb-6 lg:mb-0'>
+                    <Image src={singleProduct.image} alt={singleProduct.title} width={300} height={300} />
+                </div>
+
+                <div className='col-span-4'></div>
+            
+            </div>
         </div>
     )
 }
